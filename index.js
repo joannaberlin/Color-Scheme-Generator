ca170a05b4801af8f9ca7d22ed9e70cd6a67be53
joannaberlin <joanna.kot.ok@gmail.com>
2024-03-12 10:16:15 +0100

    Test api, get data and use it to style html dynamically

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+const colorsWrapper = document.getElementById('color-boxes_wrapper');
+let colorsArr = [];
+
 fetch('https://www.thecolorapi.com/scheme?hex=0047AB&mode=analogic')
 	.then((response) => response.json())
-	.then((data) => console.log(data));
+	.then((data) => {
+		console.log(data.colors);
+		for (let color of data.colors) {
+			colorsArr.push(color.hex.value);
+		}
+		for (let color of colorsArr) {
+			colorsWrapper.innerHTML += `<div class="color-box" style="background-color:${color}"><p>${color}</p></div>`;
+		}
+	});
+
+console.log(colorsArr);
